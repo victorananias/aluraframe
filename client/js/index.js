c053501173f93dd3f594ac5465176a7c82680038
Victor <victor.ananias8@gmail.com>
2018-02-11 21:54:15 -0200

    refiz alguns comentarios

--- client/js/index.js
+++ client/js/index.js
@@ -7,21 +7,52 @@ var campos = [
 var tbody = document.querySelector("table tbody");
 
 document.querySelector(".form").addEventLis tener("submit", function(event) {
-    // preventDefault cancela o evento,
-    // neste caso ele impede que o formulário seja submetido
-    // sem ele, o tbody não será atualizado ao submeter
+
+    /*
+    |--------------------------------------------------------------------------
+    | evento.preventDefault()
+    |--------------------------------------------------------------------------
+    |
+    | cancela o evento,
+    | neste caso ele impede que o formulário seja submetido
+    | sem ele, o tbody não será atualizado ao submeter
+    |
+    */
     event.preventDefault();
 
-    //
+    /*
+    |--------------------------------------------------------------------------
+    | createElement(elemento)
+    |--------------------------------------------------------------------------
+    |
+    | criando o elemento td
+    |
+    */
     var tr = document.createElement("tr");
 
     campos.forEach(function(campo) {
-        // criando o elemento td
         var td = document.createElement("td");
+
+        /*
+        |--------------------------------------------------------------------------
+        | elemento.textContent
+        |--------------------------------------------------------------------------
+        |
+        | propriedade referente ao conteudo do elemento html
+        |
+        */
         td.textContent = campo.value;
 
-        // adiciona o elemento td como 'filho' de tr
+        /*
+        |--------------------------------------------------------------------------
+        | appendChild(elemento)
+        |--------------------------------------------------------------------------
+        |
+        | adiciona o elemento td como 'filho' de tr
+        |
+        */
         tr.appendChild(td);
+
     });
 
     var tdVolume = document.createElement('td');
@@ -35,6 +66,13 @@ document.querySelector(".form").addEventLis tener("submit", function(event) {
     campos[1].value = 1;
     campos[2].value = 0;
 
-    // dando foco ao campo data
+    /*
+    |--------------------------------------------------------------------------
+    | elemento.focus()
+    |--------------------------------------------------------------------------
+    |
+    | direcionando o foco ao elemento data
+    |
+    */
     campos[0].focus();
 });
